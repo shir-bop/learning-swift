e33d4fe1ec9e0af17ae4db859e6f196306236602
shir-bop <76579730+shir-bop@users.noreply.github.com>
2020-12-25 16:37:28 +1000

    Create 1.operators.swift

---
+++ 1.operators.swift
@@ -0,0 +1,26 @@
+
+print(
+  5+3,
+  5-3,
+  5-2.5,
+  5/2,
+  5*2
+
+)
+var name=5
+var king=7
+print(king+name)
+let result=king*name
+print(result)
+/*var i=0
+while(i<10){
+  print(i)
+  i=1
+} */
+var countown=10
+repeat{
+  print(countown)
+  countown = 1
+}
+while countown>0
+print("Lift off!!")

7e7fb48dcfaf8addc636fa8e6d32e4531529edea
Shirin Mujeeb <76579730+shirinmujeeb@users.noreply.github.com>
2021-01-07 23:55:31 +1000

    Add files via upload

---
+++ i am poor/i am poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i am poor
+//
+//  Created by shir on 7/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
